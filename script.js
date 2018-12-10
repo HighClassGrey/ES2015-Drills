68963124f980346ea189fcd6ebe81bc5c728d671
HighClassGrey <tamekaahannah@gmail.com>
2018-12-10 13:53:50 -0600

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+// function favMovie(movie,name = 'world') {
+//     console.log (`my name is ${name} and my favorite movie is ${movie}`);
+// }
+
+// favMovie('tameka')
+
+function favMovie = (a,b) => {
+    
+}
